9cb623402e02341060b7528dc60c63943df90f99
Andrew <rainbowshardofdeath@gmail.com>
2025-02-09 15:19:14 +0400

    Fix drawing

--- ui/painter.py
+++ ui/painter.py
@@ -5,12 +5,17 @@ from OpenGL.GL import *
 class Painter:
     def draw(self, object):
         glColor3f(*np.array(object.color) / 255.0)
-        glBegin(GL_QUADS)
+        glBegin(GL_TRIANGLE_FAN)
 
+        position = object.body.position
         size = object.size
-        glVertex2f(object.body.position[0] - size, object.body.position[1] - size)
-        glVertex2f(object.body.position[0] + size, object.body.position[1] - size)
-        glVertex2f(object.body.position[0] + size, object.body.position[1] + size)
-        glVertex2f(object.body.position[0] - size, object.body.position[1] + size)
+
+        glVertex2f(position.x, position.y)
+
+        # Draw circle segments
+        num_segments = 8  # More segments for a smoother circle
+        for i in range(num_segments + 1):
+            angle = 2 * np.pi * i / num_segments
+            glVertex2f(position.x + np.cos(angle) * size, position.y + np.sin(angle) * size)
 
         glEnd()
