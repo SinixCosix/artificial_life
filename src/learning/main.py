2cab57469fd09576f123da47a53a79021b844c40
SinixCosix <rainbowshardofdeath@gmail.com>
2025-03-08 22:57:04 +0400

    Learn basic physics engine

--- src/learning/main.py
+++ src/learning/main.py
@@ -1,102 +1,124 @@
 """ Sprite Sample Program """
 
-import random
 import arcade
 
 # --- Constants ---
-SPRITE_SCALING_PLAYER = 0.1
-SPRITE_SCALING_COIN = 0.2
-COIN_COUNT = 50
+SPRITE_SCALING_BOX = 0.5
+SPRITE_SCALING_PLAYER = 0.5
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+MOVEMENT_SPEED = 5
+
 
 class MyGame(arcade.Window):
-    """ Our custom Window Class"""
+    """ This class represents the main window of the game. """
 
     def __init__(self):
         """ Initializer """
         # Call the parent class initializer
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
 
-                # Variables that will hold sprite lists.
+        # Sprite lists
         self.player_list = None
-        self.coin_list = None
-
-        # Set up the player info
-        self.player_sprite = None
-        self.score = 0
-
-        # Don't show the mouse cursor
-        self.set_mouse_visible(False)
-
-        arcade.set_background_color(arcade.color.AMAZON)
-
-    def setup(self):
-                # Sprite lists
-        self.player_list = arcade.SpriteList()
-        self.coin_list = arcade.SpriteList()
-
-        # Score
-        self.score = 0
+        self.wall_list = None
 
         # Set up the player
-        # Character image from kenney.nl
-        self.player_sprite = arcade.Sprite("zombie.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite = None
+
+        # This variable holds our simple "physics engine"
+        self.physics_engine = None
+
+    def setup(self):
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+
+        # Reset the score
+        self.score = 0
+
+        # Create the player
+        self.player_sprite = arcade.Sprite("images/character.png", SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
+        self.player_sprite.center_y = 64
         self.player_list.append(self.player_sprite)
 
-        for i in range(COIN_COUNT):
+        # --- Manually place walls
 
-            # Create the coin instance
-            # Coin image from kenney.nl
-            coin = arcade.Sprite("brain.jpeg", SPRITE_SCALING_COIN)
+        # Manually create and position a box at 300, 200
+        wall = arcade.Sprite("images/boxCrate_double.png", SPRITE_SCALING_BOX)
+        wall.center_x = 300
+        wall.center_y = 200
+        self.wall_list.append(wall)
 
-            # Position the coin
-            coin.center_x = random.randrange(SCREEN_WIDTH)
-            coin.center_y = random.randrange(SCREEN_HEIGHT)
+        # Manually create and position a box at 364, 200
+        wall = arcade.Sprite("images/boxCrate_double.png", SPRITE_SCALING_BOX)
+        wall.center_x = 364
+        wall.center_y = 200
+        self.wall_list.append(wall)
 
-            # Add the coin to the lists
-            self.coin_list.append(coin)
+        # --- Place boxes inside a loop
+        for x in range(173, 650, 64):
+            wall = arcade.Sprite("images/boxCrate_double.png", SPRITE_SCALING_BOX)
+            wall.center_x = x
+            wall.center_y = 350
+            self.wall_list.append(wall)
 
+        # --- Place walls with a list
+        coordinate_list = [[400, 500],
+                           [470, 500],
+                           [400, 570],
+                           [470, 570]]
+
+        # Loop through coordinates
+        for coordinate in coordinate_list:
+            wall = arcade.Sprite("images/boxCrate_double.png", SPRITE_SCALING_BOX)
+            wall.center_x = coordinate[0]
+            wall.center_y = coordinate[1]
+            self.wall_list.append(wall)
+
+        # Create the physics engine. Give it a reference to the player, and
+        # the walls we can't run into.
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
 
     def on_draw(self):
         self.clear()
-        self.coin_list.draw()
+        self.wall_list.draw()
         self.player_list.draw()
-        output = f"Score: {self.score}"
-        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
-
-
-    def on_mouse_motion(self, x, y, dx, dy):
-
-        self.player_sprite.center_x = x
-        self.player_sprite.center_y = y
 
     def on_update(self, delta_time):
-        """ Movement and game logic """
+        self.physics_engine.update()
 
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
-        self.coin_list.update()
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
 
-        # Generate a list of all sprites that collided with the player.
-        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
-                                                              self.coin_list)
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
 
-        # Loop through each colliding sprite, remove it, and add to the score.
-        for coin in coins_hit_list:
-            coin.remove_from_sprite_lists()
-            self.score += 1
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
 
 
 def main():
     """ Main method """
     window = MyGame()
     window.setup()
-
     arcade.run()
 
 
