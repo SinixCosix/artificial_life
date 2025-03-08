1a530374648b6973e51aca3274fbddd34d07fa58
SinixCosix <rainbowshardofdeath@gmail.com>
2025-03-08 21:17:48 +0400

    Learn basics drawing

--- src/main.py
+++ src/main.py
@@ -1,23 +1,19 @@
 import arcade
 
-from ui import MainWindow
+class Game(arcade.Window):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        arcade.set_background_color(arcade.csscolor.LIGHT_BLUE)
+
+    def on_draw():
+        arcade.draw_circle_filled(50, 50, 15, arcade.color.AUBURN)
+
+
 
 def main():
     screen = arcade.get_display_size()
     width, height = screen[0] / 3, screen[1] / 2
-    arcade.open_window(width, height)
-    arcade.set_background_color(arcade.csscolor.LIGHT_BLUE)
-
-    arcade.start_render()
-    arcade.draw_lrbt_rectangle_filled(0, width, 0, height / 3, arcade.csscolor.LIGHT_GREEN)
-    arcade.draw_polygon_filled((
-        (40, 40),
-        (45, 60),
-        (35, 89),
-        (40, 55),
-    ), arcade.csscolor.SIENNA)
-    arcade.draw_line(100, 150, 200, 150, arcade.color.YELLOW, 3)
-    arcade.finish_render()
+    window = Game(width, height)
 
     arcade.run()
 
