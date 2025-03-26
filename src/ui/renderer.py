eaec0419ef698e291a03cf5a075bbcab2e7e02ff
Andrew <rainbowshardofdeath@gmail.com>
2025-03-27 01:30:16 +0400

    Fix rendering

--- src/ui/renderer.py
+++ src/ui/renderer.py
@@ -5,28 +5,16 @@ import simulation
 
 class Renderer:
     def __init__(self):
-        self.sprite_list = arcade.SpriteList()
-        self.sprite_map = {}
+        self.shape_list = arcade.shape_list.ShapeElementList()
 
     def initialize(self, items):
         for item in items:
-            sprite = self.create_sprite(item)
-            self.sprite_list.append(sprite)
-            self.sprite_map[item] = sprite
-
-    def create_sprite(self, item):
-        sprite = arcade.SpriteSolidColor(20, 20, item.color)
-        sprite.position = item.position
-
-        return sprite
-
-    def update(self, items):
-        for item in items:
-            sprite = self.sprite_map.get(item)
-            if sprite:
-                sprite.position = item.position
-                sprite.color = item.color
+            vertices = item.shape.get_vertices()
+            shape = [item.local_to_world(vertex) for vertex in vertices]
+            shape = arcade.shape_list.create_polygon(shape, item.color)
+            self.shape_list.append(shape)
 
     def render(self, items):
-        self.update(items)
-        self.sprite_list.draw()
+        self.shape_list.clear()
+        self.initialize(items)
+        self.shape_list.draw()
